c6ce3d1295c47ec595a8e5e63ea1b3c45e4a4ade
Christopher Belanger <belanger.chris@gmail.com>
2014-03-30 13:34:04 -0400

    Got mysql on cloud9 working

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'c9',                      # Or path to database file if using sqlite3.
         'USER': 'feasoron',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.3.210.129',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
