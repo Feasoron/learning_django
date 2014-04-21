825a5e15943a600f7587f92ba8579cb1da3a1451
Chris Belanger <belanger.chris@gmail.com>
2014-04-20 23:34:45 -0400

    update before upgrade

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'c9',                      # Or path to database file if using sqlite3.
-        'USER': 'feasoron',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '127.3.210.129',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'learning_django',                      # Or path to database file if using sqlite3.
+        'USER': 'django',                      # Not used with sqlite3.
+        'PASSWORD': 'ltcdj2007',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
